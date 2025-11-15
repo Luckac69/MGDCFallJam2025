5fd803e3d40cdb2f016eafc5f75306c135c6e382
luckac69 <luckac69@gmail.com>
2025-11-15 17:08:59 -0500

    terrain generation test

--- main.gd
+++ main.gd
@@ -2,5 +2,8 @@ extends Node3D
 
 
 
-func _input(event):
-		print(event)
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
